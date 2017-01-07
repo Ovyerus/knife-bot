ecc4a091dc96faffe97b8823ce344e9b016c920a
Ovyerus <iamovyerus@gmail.com>
2017-01-07 22:03:37 +1100

    initial changes

--- knife.js
+++ knife.js
@@ -1,9 +1,11 @@
 const Eris = require('eris');
 const Promise = require('bluebird');
+const cp = require('child_process');
 const util = require('util');
 const logger = require('./logger.js');
 const prefixParse = require('./prefixParse.js');
-const knife = new Eris('MjY2MTI5ODM5MTI2MjE2NzA1.C05ZpA.4KHT-oAsS9Q27YMQ-Hn5OA4K-Co', {
+const config = require('./config.json');
+const knife = new Eris(config.token, {
     disableEvents: {
         PRESENCE_UPDATE: true,
         TYPING_START: true,
@@ -11,10 +13,10 @@ const knife = new Eris('MjY2MTI5ODM5MTI2MjE2NzA1.C05ZpA.4KHT-oAsS9Q27YMQ-Hn5OA4K
     }
 });
 
-const owner = '99742488666845184';
+knife.owner = config.owner;
+knife.redHot = '🔥 1⃣0⃣0⃣0⃣ 🌡 🔪'; 
 const prefixes = ['\uD83D\uDD2A', '<@{{id}}> '];
 const commands = {};
-const thingy = '🔥 1⃣0⃣0⃣0⃣ 🌡 🔪';
 
 knife.formatUser = user => {
     return user instanceof Eris.Member ? `${user.nick ? user.nick : user.user.username}#${user.user.discriminator}` : `${user.username}#${user.discriminator}`;
@@ -38,35 +40,52 @@ knife.on('messageCreate', msg => {
         let args = content.split(' ');
         let cmd = args.shift();
 
-        if ((cmd === 'eval' || cmd === 'cooldown') && msg.author.id === owner) {
-            commands[cmd].func(msg, args).then(() => {
-                logger.cmd(`${msg.guild.name} | ${msg.channel.name} + ${knife.formatUser(msg.author)}: ${msg.cleanContent}`);
-            }).catch(err => {
+        commands[cmd].func(msg, args).then(lul => {
+            if (!lul) logger.cmd(`${msg.guild.name} | ${msg.channel.name} + ${knife.formatUser(msg.author)}: ${msg.cleanContent}`);
+        }).catch(err => {
+            if (err.resp && err.resp.statusCode === 403) {
+                logger.warn(`Can't send message in '#${msg.channel.name}' (${msg.channel.id}), cmd from user '${knife.formatUser(msg.author)}' (${msg.author.id})`);
+                knife.getDMChannel(msg.author.id).then(dm => {
+                    knife.createMessage(dm.id, `It appears I was unable to send a message in \`#${msg.channel.name}\` on the server \`${msg.guild.name}\`. Please give me the Send Messages permission or notify a mod or admin if you cannot do this.`);
+                }).catch(() => logger.warn(`Couldn't get DM channel for ${knife.formatUser(msg.author)} (${msg.author.id})`));
+            } else {
                 logger.error(`Error in command '${cmd}' from user ${knife.formatUser(msg.author)} in #${msg.channel.name} (${msg.channel.id})\n${err}\n${err.stack}`);
-            });
-        } else if (cmd === 'eval' || cmd === 'cooldown') {
-            return;
-        } else {
-            commands[cmd].func(msg, args).then(() => {
-                logger.cmd(`${msg.guild.name} | ${msg.channel.name} + ${knife.formatUser(msg.author)}: ${msg.cleanContent}`);
-            }).catch(err => {
-                if (err.resp && err.resp.statusCode === 403) {
-                    logger.warn(`Can't send message in '#${msg.channel.name}' (${msg.channel.id}), cmd from user '${knife.formatUser(msg.author)}' (${msg.author.id})`);
-                    knife.getDMChannel(msg.author.id).then(dm => {
-                        knife.createMessage(dm.id, `It appears I was unable to send a message in \`#${msg.channel.name}\` on the server \`${msg.guild.name}\`. Please give me the Send Messages permission or notify a mod or admin if you cannot do this.`);
-                    });
-                } else {
-                    logger.error(`Error in command '${cmd}' from user ${knife.formatUser(msg.author)} in #${msg.channel.name} (${msg.channel.id})\n${err}\n${err.stack}`);
-                    var errMsg = `Error with command \`${cmd}\`\n`;
-                    errMsg += '```js\n';
-                    errMsg += `${err}\n`;
-                    errMsg += '```';
-                    knife.createMessage(msg.channel.id, errMsg);
-                }
-            });
-        }
+                var errMsg = `Error with command \`${cmd}\`\n`;
+                errMsg += '```js\n';
+                errMsg += `${err}\n`;
+                errMsg += '```';
+                knife.createMessage(msg.channel.id, errMsg);
+            }
+        });
     });
 });
+
+commands['help'] = {
+    description: 'Help command.',
+    func: msg => {
+        return new Promise((resolve, reject) => {
+            var helpMsg = '```\n';
+            for (let command in commands) {
+                let cmd = commands[command];
+                helpMsg += command;
+                cmd.usage ? helpMsg += ' ' + cmd.usage + ': ' : helpMsg += ': ';
+                helpMsg += cmd.description + '\n';
+            }
+            helpMsg += '```';
+
+            knife.createMessage(msg.channel.id, `${knife.redHot} Sending the help to your DMs.`).then(() => {
+                knife.getDMChannel(msg.author.id).then(dm => {
+                    knife.createMessage(dm.id, helpMsg).then(() => resolve()).catch(reject);
+                }).catch(() => {
+                    logger.warn(`Couldn't get DM channel for ${knife.formatUser(msg.author)} (${msg.author.id})`);
+                    knife.createMessage(msg.channel.id, 'It appears that I am unable to DM you. Maybe you have me blocked?');
+                });
+            }).catch(err => {
+                if (err.resp && err.resp.statusCode === 403) logger.warn(`Can't send message in '#${msg.channel.name}' (${msg.channel.id}), cmd from user '${knife.formatUser(msg.author)}' (${msg.author.id})`);
+            });
+        });
+    }
+}
 
 commands['ping'] = {
     description: 'Ping!',
@@ -123,31 +142,35 @@ commands['eval'] = {
     usage: '<👀>',
     func: (msg, args) => {
         return new Promise((resolve, reject) => {
-            var evalArgs = args.join(' ');
+            if (msg.author.id !== knife.owner) {
+                resolve('fagit');
+            } else {
+                var evalArgs = args.join(' ');
 
-            try {
-                var returned = eval(evalArgs);
-                var str = util.inspect(returned, {depth: 1});
-                str = str.replace(new RegExp(knife.token, 'gi'), '<TOKEN>');
+                try {
+                    var returned = eval(evalArgs);
+                    var str = util.inspect(returned, {depth: 1});
+                    str = str.replace(new RegExp(knife.token, 'gi'), '<TOKEN>');
 
-                if (str.length > 1900) {
-                    str = str.substr(0, 1897);
-                    str = str + '...';
+                    if (str.length > 1900) {
+                        str = str.substr(0, 1897);
+                        str = str + '...';
+                    }
+
+                    var sentMessage = '```js\n';
+                    sentMessage += `Input: ${evalArgs}\n\n`;
+                    sentMessage += `Output: ${str}\n`;
+                    sentMessage += '```';
+
+                    knife.createMessage(msg.channel.id, sentMessage).then(() => resolve()).catch(reject);
+                } catch(err) {
+                    var errMessage = '```js\n';
+                    errMessage += `Input: ${evalArgs}\n\n`;
+                    errMessage += `${err}\n`;
+                    errMessage += '```';
+
+                    knife.createMessage(msg.channel.id, errMessage).then(() => resolve()).catch(reject);
                 }
-
-                var sentMessage = '```js\n';
-                sentMessage += `Input: ${evalArgs}\n\n`;
-                sentMessage += `Output: ${str}\n`;
-                sentMessage += '```';
-
-                knife.createMessage(msg.channel.id, sentMessage).then(() => resolve()).catch(reject);
-            } catch(err) {
-                var errMessage = '```js\n';
-                errMessage += `Input: ${evalArgs}\n\n`;
-                errMessage += `${err}\n`;
-                errMessage += '```';
-
-                knife.createMessage(msg.channel.id, errMessage).then(() => resolve()).catch(reject);
             }
         });
     }
@@ -157,10 +180,39 @@ commands['cooldown'] = {
     description:'Turns the blowtorches off.',
     func: msg => {
         return new Promise((resolve, reject) => {
-            knife.createMessage(msg.channel.id, 'Turning off the blowtorches...').then(() => {
-                console.log('ayy lmao cya world');
-                process.exit()
-            });
+            if (msg.author.id !== knife.owner) {
+                resolve('fagit');
+            } else {
+                knife.createMessage(msg.channel.id, 'Turning off the blowtorches...').then(() => {
+                    console.log('ayy lmao cya world');
+                    process.exit()
+                });
+            }
+        });
+    }
+}
+
+commands['git'] = {
+    description: 'Git command.',
+    func: (msg, args) => {
+        return new Promise((resolve, reject) => {
+            if (args.length === 0) {
+                cp.exec('git log', (err, stdout, stderr) => {
+                    if (err) {
+                        reject(err);
+                    } else if (stderr) {
+                        reject(stderr);
+                    } else {
+                        var commits = stdout.split('commit ');
+                        console.log(commits);
+                        var first = 'commit ' + commits[0];
+                        var cmtMsg = 'Latest pulled commit: \n```\n';
+                        cmtMsg += first + '\n';
+                        cmtMsg += '```';
+                        knife.createMessage(msg.channel.id, cmtMsg).then(() => resolve()).catch(reject);
+                    }
+                });
+            }
         });
     }
 }
