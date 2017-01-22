d0c9a4b77082060d3c84de0f8f4691bd3f413846
Ovyerus <iamovyerus@gmail.com>
2017-01-22 17:21:06 +1100

    Add server invite to help and invite.

--- commands/help.js
+++ commands/help.js
@@ -11,14 +11,19 @@ exports.cmd = {
                 cmd.usage ? helpMsg += ' ' + cmd.usage + ': ' : helpMsg += ': ';
                 helpMsg += cmd.description + '\n';
             }
-            helpMsg += '```';
+            helpMsg += '```\nSupport server link: https://discord.gg/hD8xZMG';
 
             knife.createMessage(msg.channel.id, `${knife.redHot} Sending the help to your DMs.`).then(() => {
                 knife.getDMChannel(msg.author.id).then(dm => {
-                    knife.createMessage(dm.id, helpMsg).then(() => resolve()).catch(reject);
+                    knife.createMessage(dm.id, helpMsg).then(() => resolve()).catch(() => {
+                        knife.logger.warn(`Couldn't get DM channel for ${knife.formatUser(msg.author)} (${msg.author.id})`);
+                        knife.createMessage(msg.channel.id, 'It appears that I am unable to DM you. Maybe you have me blocked?');
+                        resolve();
+                    });
                 }).catch(() => {
                     knife.logger.warn(`Couldn't get DM channel for ${knife.formatUser(msg.author)} (${msg.author.id})`);
                     knife.createMessage(msg.channel.id, 'It appears that I am unable to DM you. Maybe you have me blocked?');
+                    resolve();
                 });
             }).catch(err => {
                 if (err.resp && err.resp.statusCode === 403) logger.warn(`Can't send message in '#${msg.channel.name}' (${msg.channel.id}), cmd from user '${knife.formatUser(msg.author)}' (${msg.author.id})`);
--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ exports.cmd = {
     description: 'Invite me to your server.',
     func: (knife, msg) => {
         return new Promise((resolve, reject) => {
-            knife.createMessage(msg.channel.id, `https://discordapp.com/oauth2/authorize?client_id=${knife.user.id}&scope=bot&permissions=388167`).then(() => resolve()).catch(reject);
+            knife.createMessage(msg.channel.id, `**OAuth Invite URL:** https://discordapp.com/oauth2/authorize?client_id=${knife.user.id}&scope=bot&permissions=38816\n**Support Server Invite:** https://discord.gg/hD8xZMG`).then(() => resolve()).catch(reject);
         });
     }
 }
