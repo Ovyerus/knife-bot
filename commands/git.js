459241cf501bf5e8898765e51d1f3c2b982afe5f
Ovyerus <iamovyerus@gmail.com>
2017-01-25 13:40:41 +1100

    Make git command use plain text if cant post embed

--- commands/git.js
+++ commands/git.js
@@ -1,4 +1,5 @@
 const Promise = require('bluebird');
+const moment = require('moment');
 const cp = require('child_process');
 const fs = require('fs');
 
@@ -10,6 +11,7 @@ exports.cmd = {
                 let commits = cp.execSync('git log');
                 let commit = commits.toString('utf8').split('Author: ')[1];
                 let hash = commits.toString('utf8').split('Author: ')[0].substring(6).trim();
+                let commitTime = Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, ''));
                 knife.createMessage(msg.channel.id, {
                     embed: {
                         color: 0x1CABB3,
@@ -20,15 +22,27 @@ exports.cmd = {
                             {name: 'Author', value: commit.split('<')[0].trim()},
                             {name: 'Message', value: commit.split('\n\n')[1].trim()}
                         ],
-                        footer: {text: 'Commit Date'},
-                        timestamp: new Date(Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, '')))
+                        footer: {text: `Commited on ${moment(commitTime).format('dddd Do MMMM Y')} at ${moment(commitTime).format('HH:mm:ss A')}`}
                     }
-                }).then(() => resolve()).catch(reject);
+                }).then(() => resolve()).catch(err => {
+                    if (err.resp && err.resp.statusCode === 400) {
+                        let m = '__**Latest local commit**__\n';
+                        m += '**GitHub Repo:** <https://github.com/Ovyerus/knife-bot>\n';
+                        m += `**Commit Hash:** ${hash}\n`;
+                        m += `**Author:** ${commit.split('<')[0].trim()}\n`;
+                        m += `**Message:** ${commit.split('\n\n')[1].trim()}\n`;
+                        m += `**Commit Time**: ${moment(commitTime).format('dddd Do MMMM Y')} at ${moment(commitTime).format('HH:mm:ss A')}`;
+                        knife.createMessage(msg.channel.id, m).then(() => resolve()).catch(reject);
+                    } else {
+                        reject(err);
+                    }
+                });
             } else if (args[0] === 'pull') {
                 if (msg.author.id !== knife.owner) {
                     let commits = cp.execSync('git log');
                     let commit = commits.toString('utf8').split('Author: ')[1];
                     let hash = commits.toString('utf8').split('Author: ')[0].substring(6).trim();
+                    let commitTime = Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, ''));
                     knife.createMessage(msg.channel.id, {
                         embed: {
                             color: 0x1CABB3,
@@ -39,10 +53,21 @@ exports.cmd = {
                                 {name: 'Author', value: commit.split('<')[0].trim()},
                                 {name: 'Message', value: commit.split('\n\n')[1].trim()}
                             ],
-                            footer: {text: 'Commit Date'},
-                            timestamp: new Date(Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, '')))
+                            footer: {text: `Commited on ${moment(commitTime).format('dddd Do MMMM Y')} at ${moment(commitTime).format('HH:mm:ss A')}`}
                         }
-                    }).then(() => resolve()).catch(reject);
+                    }).then(() => resolve()).catch(err => {
+                        if (err.resp && err.resp.statusCode === 400) {
+                            let m = '__**Latest local commit**__\n';
+                            m += '**GitHub Repo:** <https://github.com/Ovyerus/knife-bot>\n';
+                            m += `**Commit Hash:** ${hash}\n`;
+                            m += `**Author:** ${commit.split('<')[0].trim()}\n`;
+                            m += `**Message:** ${commit.split('\n\n')[1].trim()}\n`;
+                            m += `**Commit Time**: ${moment(commitTime).format('dddd Do MMMM Y')} at ${moment(commitTime).format('HH:mm:ss A')}`;
+                            knife.createMessage(msg.channel.id, m).then(() => resolve()).catch(reject);
+                        } else {
+                            reject(err);
+                        }
+                    });
                 } else {
                     cp.exec('git pull origin master', (err, stdout, stderr) => {
                         if (err) {
@@ -51,6 +76,7 @@ exports.cmd = {
                             let commits = cp.execSync('git log');
                             let commit = commits.toString('utf8').split('Author: ')[1];
                             let hash = commits.toString('utf8').split('Author: ')[0].substring(6).trim();
+                            let commitTime = Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, ''));
                             knife.createMessage(msg.channel.id, {
                                 content: 'Pulled latest commit and ready to restart.',
                                 embed: {
@@ -62,10 +88,21 @@ exports.cmd = {
                                         {name: 'Author', value: commit.split('<')[0].trim()},
                                         {name: 'Message', value: commit.split('\n\n')[1].trim()}
                                     ],
-                                    footer: {text: 'Commit Date'},
-                                    timestamp: new Date(Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, '')))
+                                    footer: {text: `Commited on ${moment(commitTime).format('dddd Do MMMM Y')} at ${moment(commitTime).format('HH:mm:ss A')}`}
                                 }
-                            }).then(() => resolve()).catch(reject);
+                            }).then(() => resolve()).catch(err => {
+                                if (err.resp && err.resp.statusCode) {
+                                    let m = '__**Latest local commit**__\n';
+                                    m += '**GitHub Repo:** <https://github.com/Ovyerus/knife-bot>\n';
+                                    m += `**Commit Hash:** ${hash}\n`;
+                                    m += `**Author:** ${commit.split('<')[0].trim()}\n`;
+                                    m += `**Message:** ${commit.split('\n\n')[1].trim()}\n`;
+                                    m += `**Commit Time**: ${moment(commitTime).format('dddd Do MMMM Y')} at ${moment(commitTime).format('HH:mm:ss A')}`;
+                                    knife.createMessage(msg.channel.id, m).then(() => resolve()).catch(reject);
+                                } else {
+                                    reject(err);
+                                }
+                            });
                         }
                     });
                 }
