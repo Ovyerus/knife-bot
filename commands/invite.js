3472af6c3a8d4140356c6be8910ac64b352279b1
Ovyerus <iamovyerus@gmail.com>
2017-01-29 20:00:56 +1100

    Update permission number in invite.

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ exports.cmd = {
     description: 'Invite me to your server.',
     func: (knife, msg) => {
         return new Promise((resolve, reject) => {
-            knife.createMessage(msg.channel.id, `**OAuth Invite URL:** https://discordapp.com/oauth2/authorize?client_id=${knife.user.id}&scope=bot&permissions=38816\n**Support Server Invite:** https://discord.gg/hD8xZMG`).then(() => resolve()).catch(reject);
+            knife.createMessage(msg.channel.id, `**OAuth Invite URL:** https://discordapp.com/oauth2/authorize?client_id=${knife.user.id}&scope=bot&permissions=201714790\n**Support Server Invite:** https://discord.gg/hD8xZMG`).then(() => resolve()).catch(reject);
         });
     }
 }
