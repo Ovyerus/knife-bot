6e20ee7a4531642483df4ea7918cad3f459c61b8
Ovyerus <iamovyerus@gmail.com>
2017-02-05 20:07:05 +1100

    Revive cooldown to drain rethink connection pool

---
+++ commands/cooldown.js
@@ -0,0 +1,31 @@
+const Promise = require('bluebird');
+
+exports.cmd = {
+    description: 'Clean up and shutdown the bot.',
+    owner: true,
+    func(knife, msg) {
+        return new Promise((resolve, reject) => {
+            if (msg.author.id !== knife.owner) {
+                resolve('o');
+            } else {
+                knife.createMessage(msg.channel.id, 'Are you sure you want to shutdown [y/n]?').then(() => {
+                    knife.awaitMessage(msg).then(m => {
+                        if (/^y(es)?$/i.test(m.content)) {
+                            knife.createMessage(m.channel.id, 'Turning off the blowtorches...').then(() => {
+                                knife.db.getPoolMaster().drain();
+                                knife.logger.info('Restarting/Exiting process...');
+                                process.exit();
+                            }).catch(reject);
+                        } else if (/^no?$/i.test(m.content)) {
+                            knife.createMessage(m.channel.id, 'Keeping the blowtorches running.').then(() => resolve()).catch(reject);
+                        } else {
+                            knife.createMessage(m.channel.id, 'That is not a valid option.').then(() => resolve()).catch(reject);
+                        }
+                    }).catch(err => {
+                        reject(err);
+                    });
+                }).catch(reject);
+            }
+        });
+    }
+}
