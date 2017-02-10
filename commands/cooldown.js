75bb201b321ad6c2ca958f3d2cc4ff8e88bce2e6
Ovyerus <iamovyerus@gmail.com>
2017-02-10 22:02:04 +1100

    Remove callback hell in cooldown

--- commands/cooldown.js
+++ commands/cooldown.js
@@ -9,21 +9,23 @@ exports.cmd = {
                 resolve('o');
             } else {
                 knife.createMessage(msg.channel.id, 'Are you sure you want to shutdown [y/n]?').then(() => {
-                    knife.awaitMessage(msg.channel.id, msg.author.id).then(m => {
-                        if (/^y(es)?$/i.test(m.content)) {
-                            knife.createMessage(m.channel.id, 'Turning off the blowtorches...').then(() => {
-                                knife.db.getPoolMaster().drain();
-                                knife.logger.info('Restarting/Exiting process...');
-                                process.exit();
-                            }).catch(reject);
-                        } else if (/^no?$/i.test(m.content)) {
-                            knife.createMessage(m.channel.id, 'Keeping the blowtorches running.').then(() => resolve()).catch(reject);
-                        } else {
-                            knife.createMessage(m.channel.id, 'That is not a valid option.').then(() => resolve()).catch(reject);
-                        }
-                    }).catch(err => {
-                        reject(err);
-                    });
+                    return knife.awaitMessage(msg.channel.id, msg.author.id);
+                }).then(m => {
+                    if (/^y(es)?$/i.test(m.content)) {
+                        return knife.createMessage(m.channel.id, 'Turning off the blowtorches...');
+                    } else if (/^no?$/i.test(m.content)) {
+                        return knife.createMessage(m.channel.id, 'Keeping the blowtorches running.');
+                    } else {
+                        return knife.createMessage(m.channel.id, 'That is not a valid option.')
+                    }
+                }).then(m => {
+                    if (m.content === 'Turning off the blowtorches...') {
+                        knife.db.getPoolMaster().drain();
+                        knife.logger.info('Restarting/Exiting process...');
+                        process.exit();
+                    } else {
+                        resolve()
+                    }
                 }).catch(reject);
             }
         });
