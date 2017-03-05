435e1eb24a7926d7fd7cf1e9a494aaf589d782d1
Ovyerus <iamovyerus@gmail.com>
2017-03-05 16:04:15 +1100

    Redo help

--- commands/help.js
+++ commands/help.js
@@ -2,35 +2,58 @@ const Promise = require('bluebird');
 
 exports.cmd = {
     description: 'Help command.',
-    func(knife, msg) {
+    func(knife, msg, args) {
         return new Promise((resolve, reject) => {
-            var helpMsg = '```\n';
-            for (let command in knife.commands) {
-                let cmd = knife.commands[command];
-                helpMsg += command;
-                cmd.usage ? helpMsg += ' ' + cmd.usage + ': ' : helpMsg += ': ';
-                helpMsg += cmd.description + '\n';
-            }
-            helpMsg += '```\nSupport server link: https://discord.gg/hD8xZMG';
+            if (args.length === 0) {
+                let embedTemplate = {title: `${knife.user.username} Help`, color: 2201331};
+                let cmdFields = [];
 
-            if (msg.channel.guild) {
-                knife.createMessage(msg.channel.id, `${knife.redHot} Sending the help to your DMs.`).then(() => {
-                    knife.getDMChannel(msg.author.id).then(dm => {
-                        knife.createMessage(dm.id, helpMsg).then(() => resolve()).catch(() => {
-                            knife.logger.warn(`Couldn't get DM channel for ${knife.formatUser(msg.author)} (${msg.author.id})`);
-                            knife.createMessage(msg.channel.id, 'It appears that I am unable to DM you. Maybe you have me blocked?');
-                            resolve();
-                        });
-                    }).catch(() => {
-                        knife.logger.warn(`Couldn't get DM channel for ${knife.formatUser(msg.author)} (${msg.author.id})`);
-                        knife.createMessage(msg.channel.id, 'It appears that I am unable to DM you. Maybe you have me blocked?');
-                        resolve();
-                    });
-                }).catch(err => {
-                    if (err.resp && err.resp.statusCode === 403) knife.logger.warn(`Can't send message in '#${msg.channel.name}' (${msg.channel.id}), cmd from user '${knife.formatUser(msg.author)}' (${msg.author.id})`);
-                });
+                for (let cmd in knife.commands) {
+                    let cmnd = knife.commands[cmd];
+                    cmdFields.push({name: cmd, value: `${cmnd.usage ? `${cmnd.usage} - ` : ''}${cmnd.description}`});
+                }
+
+                if (msg.channel.guild) {
+                    knife.createMessage(msg.channel.id, 'Sending the help message to your DMs').then(() => {
+                        return knife.getDMChannel(msg.author.id);
+                    }).then(dm => {
+                        let fieldCollect = [];
+                        let msgs = [];
+                        for (let i in cmdFields) {
+                            fieldCollect.push(cmdFields[i]);
+                            if ((Number(i) % 24 === 0 && Number(i) !== 0) || Number(i) === cmdFields.length - 1) {
+                                let embed = embedTemplate;
+                                embed.fields = fieldCollect;
+                                fieldCollect = [];
+                                msgs.push(knife.createMessage(dm.id, {embed}));
+                            }
+                        }
+
+                        return Promise.all(msgs);
+                    }).then(() => resolve()).catch(reject);
+                } else {
+                    let fieldCollect = [];
+                    let msgs = [];
+                    for (let i in cmdFields) {
+                        fieldCollect.push(cmdFields[i]);
+                        if ((Number(i) % 24 === 0 && Number(i) !== 0) || Number(i) === cmdFields.length - 1) {
+                            let embed = embedTemplate;
+                            embed.fields = fieldCollect;
+                            fieldCollect = [];
+                            msgs.push(knife.createMessage(msg.channel.id, {embed}));
+                        }
+                    }
+
+                    Promise.all(msgs).then(() => resolve()).catch(reject);
+                }
             } else {
-                knife.createMessage(msg.channel.id, helpMsg).then(() => resolve()).catch(reject);
+                if (!knife.commands[args[0]]) {
+                    knife.createMessage(msg.channel.id, 'That command does not exist. Make sure to check your spelling.').then(() => resolve()).catch(reject);
+                } else {
+                    let cmd = knife.commands[args[0]];
+                    let embed = {title: args[0], description: `${cmd.usage ? `\`${cmd.usage}\` - `: ''}**${cmd.description}**`, color: 2201331};
+                    knife.createMessage(msg.channel.id, {embed}).then(() => resolve()).catch(reject);
+                }
             }
         });
     }
