773db95ab30bc801b8a3f7cabbc7b6d00a46bdc6
Ovyerus <iamovyerus@gmail.com>
2017-04-01 19:48:34 +1100

    Re-do git command embed

--- src/commands/git/git.js
+++ src/commands/git/git.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
 const cp = require('child_process');
+const got = require('got');
 
 exports.loadAsSubcommands = true;
 exports.commands = ['pull'];
@@ -9,21 +10,32 @@ exports.main = {
     permission: {node: 'git'},
     main(bot, ctx) {
         return new Promise((resolve, reject) => {
+            let outer = {};
             log().then(commits => {
                 let [hash, commit] = commits.split('Author: ');
-                hash = hash.substring(6).trim();
-                let commitTime = moment(Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, '')));
+                outer.commit = commit;
+                outer.hash = hash.substring(6).trim().substring(0, 6);
+                outer.commitTime = moment(Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, '')));
+                outer.author = commit.split('<')[0].trim();
+
+                return got(`https://api.github.com/users/${outer.author}`);
+            }).then(res => {
+                let user = JSON.parse(res.body);
 
                 return ctx.createMessage({embed: {
                     color: 0x1CABB3,
-                    title: 'Latest local commit',
-                    description: '[GitHub Repo](https://github.com/Ovyerus/knife-bot)',
+                    title: 'GitHub Repository',
+                    url: 'https://github.com/Ovyerus/knife-bot',
+                    author: {
+                        name: outer.author,
+                        url: user.html_url,
+                        icon_url: user.avatar_url
+                    },
+
                     fields: [
-                        {name: 'Commit Hash', value: hash},
-                        {name: 'Author', value: commit.split('<')[0].trim()},
-                        {name: 'Message', value: commit.split('\n\n')[1].trim()}
+                        {name: `\`${outer.hash}\``, value: outer.commit.split('\n\n')[1].trim()}
                     ],
-                    footer: {text: `Commited on ${commitTime.format('dddd Do MMMM Y')} at ${commitTime.format('HH:mm:ss A')}`}
+                    footer: {text: `Commited on ${outer.commitTime.format('dddd Do MMMM Y')} at ${outer.commitTime.format('HH:mm:ss A')}`}
                 }});
             }).then(resolve).catch(reject);
         });
@@ -34,25 +46,33 @@ exports.pull = {
     desc: 'Pull the latest changes from GitHub',
     owner: true,
     main(bot, ctx) {
+        let outer = {};
         pull().then(log).then(commits => {
             let [hash, commit] = commits.split('Author: ');
-            hash = hash.substring(6).trim();
-            let commitTime = moment(Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, '')));
+            outer.commit = commit;
+            outer.hash = hash.substring(6).trim().substring(0, 6);
+            outer.commitTime = moment(Date.parse(commit.split('\n\n')[0].split('\n')[1].replace(/Date:\s+/, '')));
+            outer.author = commit.split('<')[0].trim();
 
-            return ctx.createMessage({
-                content: 'Pulled latest commit and ready to restart.',
-                embed: {
-                    color: 0x1CABB3,
-                    title: 'Latest local commit',
-                    description: '[GitHub Repo](https://github.com/Ovyerus/knife-bot)',
-                    fields: [
-                        {name: 'Commit Hash', value: hash},
-                        {name: 'Author', value: commit.split('<')[0].trim()},
-                        {name: 'Message', value: commit.split('\n\n')[1].trim()}
-                    ],
-                    footer: {text: `Commited on ${commitTime.format('dddd Do MMMM Y')} at ${commitTime.format('HH:mm:ss A')}`}
-                }
-            });
+            return got(`https://api.github.com/users/${outer.author}`);
+        }).then(res => {
+            let user = JSON.parse(res.body);
+
+            return ctx.createMessage({embed: {
+                color: 0x1CABB3,
+                title: 'GitHub Repository',
+                url: 'https://github.com/Ovyerus/knife-bot',
+                author: {
+                    name: outer.author,
+                    url: user.html_url,
+                    icon_url: user.avatar_url
+                },
+
+                fields: [
+                    {name: `\`${outer.hash}\``, value: outer.commit.split('\n\n')[1].trim()}
+                ],
+                footer: {text: `Commited on ${outer.commitTime.format('dddd Do MMMM Y')} at ${outer.commitTime.format('HH:mm:ss A')}`}
+            }});
         }).then(resolve).catch(reject);
     }
 };
