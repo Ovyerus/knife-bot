d5df9a2ed86514e79f91cc71cf9934be9d166604
Ovyerus <iamovyerus@gmail.com>
2017-04-19 19:16:11 +1000

    Update invite permissions

--- src/commands/invite/invite.js
+++ src/commands/invite/invite.js
@@ -5,7 +5,7 @@ exports.invite = {
     permission: {node: 'general.invite'},
     main(bot, ctx) {
         return new Promise((resolve, reject) => {
-            ctx.createMessage(`**OAuth Invite URL (Recommended Permissions):** https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=201714790\n**Support Server:** https://discord.gg/hD8xZMG`).then(resolve).catch(reject);
+            ctx.createMessage(`**OAuth Invite URL (Recommended Permissions):** https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=470150215\n**Support Server:** https://discord.gg/hD8xZMG`).then(resolve).catch(reject);
         });
     }
 };
