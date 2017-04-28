0b189af7bf814cc4165270e28cc62d6701a4e7d8
Ovyerus <iamovyerus@gmail.com>
2017-04-28 17:30:47 +1000

    New help message

--- src/commands/help/help.js
+++ src/commands/help/help.js
@@ -1,34 +1,54 @@
+/*
+ * help.js - Display information for all avaliable commands.
+ *
+ * Contributed by Capuccino and Ovyerus.
+ */
+
+/* eslint-env node */
+
+
 exports.commands = ['help'];
 
 exports.help = {
-    desc: 'Help command.',
-    fullDesc: 'Displays help about all the commands, and optionally for a single command.', 
+    desc: 'Show bot help.',
     usage: '[command]',
     main(bot, ctx) {
         return new Promise((resolve, reject) => {
-            if (ctx.args.length === 0) {
-                let embedTemplate = {title: `${bot.user.username} Help`, color: 0xCDDC39};
-                let cmdFields = [];
-
+            if (!ctx.args[0]) {
+                let cmds = [];
+                
                 bot.commands.forEach((cmd, name) => {
-                    if (ctx.author.id !== bot.config.owner && cmd.owner) { //
-
+                    if (cmd.owner && bot.isOwner(ctx.author.id)) {
+                        cmds.push(`🔪 ${name}${cmd.usage ? ` ${cmd.usage}` : ''}`);
+                    } else if (cmd.owner) {
+                        return;
                     } else {
-                        cmdFields.push({name: name, value: `${cmd.usage ? `${cmd.usage} - ` : ''}${cmd.desc}`, inline: true});
+                        cmds.push(`🔪 ${name}${cmd.usage ? ` ${cmd.usage}` : ''}`);
                     }
                 });
 
                 ctx.createMessage(`${bot.redHot} Slicing into your DMs!`).then(() => {
-                    let fieldCollect = [];
+                    let cmdCollect = [];
                     let msgs = [];
-                    
-                    for (let i in cmdFields) {
-                        fieldCollect.push(cmdFields[i]);
-                        if ((Number(i) % 24 === 0 && Number(i) !== 0) || Number(i) === cmdFields.length - 1) {
-                            let embed = embedTemplate;
-                            embed.fields = fieldCollect;
-                            fieldCollect = [];
-                            console.log(embed);
+
+                    for (let i in cmds) {
+                        cmdCollect.push(cmds[i]);
+                        if (i === '29' || Number(i) === cmds.length - 1) {
+                            let embed = new embedTemplate(bot);
+                            embed.title = bot.isOwner(ctx.author.id) ? `${bot.commands.length} Commands` : `${bot.commands.length - bot.commands.filter(cmd => cmd.owner).length} Commands`;
+
+                            if (cmdCollect.length > 15) {
+                                embed.fields[0].value = `\`${cmdCollect.slice(0, 15).join('\n')}\``;
+                                embed.fields[1] = {
+                                    name: '\u200b',
+                                    value: `\`${cmdCollect.slice(15, 30).join('\n')}\``,
+                                    inline: true
+                                };
+                            } else {
+                                embed.fields[0].value = `\`${cmdCollect.join('\n')}\``;
+                            }
+
+                            cmdCollect = [];
                             msgs.push(ctx.createMessage({embed}, null, 'author'));
                         }
                     }
@@ -36,14 +56,12 @@ exports.help = {
                     return Promise.all(msgs);
                 }).then(resolve).catch(reject);
             } else {
-                if (!bot.commands.checkCommand(ctx.args[0])) {
-                    ctx.createMessage('That command does not exist. Make sure to check your spelling.').then(resolve).catch(reject);
+                if (!bot.commands.getCommand(ctx.args[0])) {
+                    ctx.createMessage(`Command \`${ctx.args[0]}\` could not be found. Make sure to check your spelling.`);
                 } else {
                     let cmd = bot.commands.getCommand(ctx.args[0]);
                     let embed = {
-                        title: ctx.args[0],
-                        description: `${cmd.usage ? `\`${cmd.usage}\` - `: ''}**${cmd.desc}**`,
-                        color: 0xCDDC39
+                        description: `\`🔪 ${ctx.args[0]}${cmd.usage ? ` ${cmd.usage}` : ''}\n\u200b - ${cmd.desc}\``
                     };
 
                     ctx.createMessage({embed}).then(resolve).catch(reject);
@@ -52,3 +70,12 @@ exports.help = {
         });
     }
 };
+
+// TFW cant force JS to gimme a new object when referring to others
+function embedTemplate(bot) {
+    this.author = {name: `${bot.user.username} Help`};
+    this.description = 'Run `🔪 help command` to get more information on how to use the command and what it does.'
+    this.fields = [
+        {name: '\u200b', inline: true}
+    ];
+}
