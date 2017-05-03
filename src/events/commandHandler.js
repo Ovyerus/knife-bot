6d489c5c44c626a0db1793bffcb19b17f42a43a0
Ovyerus <iamovyerus@gmail.com>
2017-05-03 17:23:25 +1000

    New error message

--- src/events/commandHandler.js
+++ src/events/commandHandler.js
@@ -8,7 +8,7 @@ module.exports = bot => {
         if (!bot.useCommands || !msg.author || msg.author.id === bot.user.id) return;
 
         if (!msg.channel.guild) {
-            logger.custom('cyan', 'dm', loggerPrefix(msg) + msg.cleanContent);
+            logger.custom('cyan', 'dm', `${loggerPrefix(msg)}${msg.cleanContent}`);
             return;
         }
 
@@ -66,28 +66,70 @@ module.exports = bot => {
      * @param {Object} err The error object to analyse.
      */
     function handleCmdErr(msg, cmd, err) {
-        if (err.response && typeof err.response === 'string') var resp = JSON.parse(err.response);
-        if (resp && resp.code === 50013) {
-            logger.warn(`Can't send message in '#${msg.channel.name}' (${msg.channel.id}), cmd from user '${
-            bot.formatUser(msg.author)}' (${msg.author.id})`);
-            msg.author.getDMChannel().then(dm => {
-                console.log(dm.id);
-                return dm.createMessage(`It appears I was unable to send a message in \`#${msg.channel.name}\` on the server \`${msg.channel.guild.name}\`. Please give me the Send Messages permission or notify a mod or admin if you cannot do this.`);
-            }).catch(() => logger.warn(`Couldn't get DM channel for/send DM to ${bot.formatUser(msg.author)} (${msg.author.id})`));
-        } else if (resp && /\{'code':.+, 'message':.+\}/.test(err.response) && resp.code !== 50013) {
-            logger.warn(loggerPrefix(msg) + `Discord error running command "${cmd}":\n:${err.stack}`); // eslint-disable-line prefer-template
-            let m = `Discord error while trying to execute \`${cmd}\`\n`;
-            m += '```js\n';
-            m += `Code: ${resp.code}. Message: ${resp.message}\n`;
-            m += "``` If you feel this shouldn't be happening, join my support server. Invite can be found in the `invite` command.";
-            msg.channel.createMessage(m);
-        } else {
-            logger.error(loggerPrefix(msg) + `Error running command "${cmd}":\n${err.stack}`); // eslint-disable-line prefer-template
-            let m = `Experienced error while executing \`${cmd}\`\n`;
-            m += '```js\n';
-            m += err + '\n'; // eslint-disable-line prefer-template
-            m += "``` If you feel this shouldn't be happening, join my support server. Invite can be found in the `invite` command.";
-            msg.channel.createMessage(m);
-        }
+        return new Promise((resolve, reject) => {
+            let resp = typeof err.response === 'string' && /^\{'code':\d+, 'message':.*\}$/.test(err.response) ? JSON.parse(err.response) : null;
+
+            if (resp && resp.code === 50013 && !msg.channel.guild.members.get(bot.user.id).permissions.has('sendMessages')) {
+                logger.warn(`Can't send message in '#${msg.channel.name}' (${msg.channel.id}), cmd from user '${bot.formatUser(msg.author)}' (${msg.author.id})`);
+
+                msg.author.getDMChannel().then(dm => {
+                    console.log(dm.id);
+                    return dm.createMessage(`It appears I was unable to send a message in \`#${msg.channel.name}\` on the server \`${msg.channel.guild.name}\`.\nPlease give me the Send Messages permission or notify a mod or admin if you cannot do this.`);
+                }).then(resolve).catch(() => logger.warn(`Couldn't get DM channel for/send DM to ${bot.formatUser(msg.author)} (${msg.author.id})`));
+            } else if (resp && resp.code !== 50013) {
+                logger.warn(`${loggerPrefix(msg)}Discord error while running command "${cmd}":\n${err.stack}`);
+                    
+                let embed = {
+                    title: 'Error',
+                    description: 'A Discord error occurred while trying to execute command `${ctx.cmd}`',
+                    color: 0xF44336,
+                    timestamp: new Date(),
+                    footer: {text: 'Powered by Knife Bot'},
+                    fields: [
+                        {
+                            name: '\u200b',
+                            value: '```js\n'
+                            + `Code: ${resp.code}\n`
+                            + `Message: ${resp.message}\n`
+                            + '```'
+                            + 'This has been logged, but if you wish to report this now so it can get fixed faster, you can join my [**support server**](https://discord.gg/oneshot).'
+                        }
+                    ]
+                };
+
+                if (!bot.hasPermission('embedLinks', msg.channel)) {
+                    let content = bot.flattenEmbed(embed);
+                    msg.channel.createMessage(content).then(resolve).catch(reject);
+                } else {
+                    msg.channel.createMessage({embed}).then(resolve).catch(reject);
+                }
+            } else {
+                logger.error(`${loggerPrefix(msg)}Error running command "${cmd}":\n${err.stack}`);
+
+                let embed = {
+                    title: 'Error',
+                    description: 'An error occurred while trying to execute command `${ctx.cmd}`',
+                    color: 0xF44336,
+                    timestamp: new Date(),
+                    footer: {text: 'Powered by Knife Bot'},
+                    fields: [
+                        {
+                            name: '\u200b',
+                            value: '```js\n'
+                            + `${err}\n`
+                            + '```'
+                            + 'This has been logged, but if you wish to report this now so it can get fixed faster, you can join my [**support server**](https://discord.gg/oneshot).'
+                        }
+                    ]
+                };
+
+                if (!bot.hasPermission('embedLinks', msg.channel)) {
+                    let content = bot.flattenEmbed(embed);
+                    msg.channel.createMessage(content).then(resolve).catch(reject);
+                } else {
+                    msg.channel.createMessage({embed}).then(resolve).catch(reject);
+                }
+            }
+        });
     }
 };
