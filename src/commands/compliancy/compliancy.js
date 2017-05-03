729ce08b7e64f84eae18a562c1e596fc5b4de5e9
Ovyerus <iamovyerus@gmail.com>
2017-05-03 17:23:37 +1000

    Add hidden compliancy command

---
+++ src/commands/compliancy/compliancy.js
@@ -0,0 +1,14 @@
+exports.commands = ['compliancy'];
+
+exports.compliancy = {
+    desc: '\u200b',
+    hidden: true,
+    main(bot, ctx) {
+        return ctx.createMessage('Commands can stll be run even with a few select wrappers or prefixes.\n\n'
+        + 'Wrappers are either `[prefix command]` or `{prefix command}` but not both at the same time.\n'
+        + 'Prefixes are either `~` or `//` but not both at the same time.\n\n'
+        + "I don't know why I implemented this.\n\n"
+        + '**Will Work**: [:knife: ping], //:knife: ping\n'
+        + "**Won't Work**: [:knife: ping}, [{:knife: ping}], [//:knife: ping]");
+    }
+};
