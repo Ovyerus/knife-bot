1c46f61e0703f92781a9cbfcef9b78c8acd41d95
Ovy <Ovyerus@users.noreply.github.com>
2017-05-03 14:58:07 +1000

    Shorten invite code and hide embed.

--- src/commands/invite/invite.js
+++ src/commands/invite/invite.js
@@ -2,10 +2,8 @@ exports.commands = ['invite'];
 
 exports.invite = {
     desc: 'Invite me to your server.',
-    permission: {node: 'general.invite'},
     main(bot, ctx) {
-        return new Promise((resolve, reject) => {
-            ctx.createMessage(`**OAuth Invite URL (Recommended Permissions):** https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=470150215\n**Support Server:** https://discord.gg/hD8xZMG`).then(resolve).catch(reject);
-        });
+        return ctx.createMessage(`**OAuth Invite URL (Recommended Permissions):** <https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=470150215>\n'
+        + '**Support Server:** https://discord.gg/hD8xZMG`);
     }
 };
