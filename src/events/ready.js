234d238eb93b44e139f1ff70b11b533a6d95d1cb
Ovyerus <iamovyerus@gmail.com>
2017-06-12 20:28:24 +1000

    Move ready event to async await bc I forgot

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,39 +1,33 @@
 module.exports = bot => {
-    bot.on('ready', () => {
+    bot.on('ready', async () => {
         if (bot.loadCommands) {
             bot.config.prefixes.forEach((prefix, i) => {
                 if (~prefix.indexOf('{{id}}')) bot.config.prefixes[i] = prefix.replace(/\{\{id\}\}/g, bot.user.id);
             });
 
-            let outer;
-            require(`${__baseDir}/modules/commandLoader`).init(bot).then(() => {
-                logger.info(`Loaded ${bot.commands.length} ${bot.commands.length === 1 ? 'command' : 'commands'}.`);
-                return bot.db.tableList().run();
-            }).then(res => {
-                outer = res;
-                if (!~res.indexOf('guild_settings')) {
-                    logger.info('Setting up "guild_settings" table in database.');
-                    return bot.db.tableCreate('guild_settings').run();
-                }
+            await require(`${__baseDir}/modules/commandLoader`).init(bot);
+            logger.info(`Loaded ${bot.commands.length} ${bot.commands.length === 1 ? 'command' : 'commands'}.`);
 
-                return null;
-            }).then(() => {
-                if (!~outer.indexOf('strikes')) {
-                    logger.info('Setting up "strikes" table in database.');
-                    return bot.db.tableCreate('strikes').run();
-                }
+            let tableList = await  bot.db.tableList().run();
 
-                return null;
-            }).then(() => {
-                bot.loadCommands = false;
-                bot.useCommands = true;
+            if (!tableList.includes('guild_settings')) {
+                logger.info('Setting up "guild_settings" table in database.');
+                await bot.db.tableCreate('guild_settings').run();
+            }
 
-                logger.info(`${bot.user.username} is online and ready to cut shit I guess.`);
-                logger.info(`-- ${bot.guilds.size} guilds, ${Object.keys(bot.channelGuildMap).length} channels, ${bot.users.size} users --`);
-                logger.info(`-- ${bot.shards.size} shards`);
-            });
+            if (!tableList.includes('strikes')) {
+                logger.info('Setting up "strikes" table in database.');
+                await bot.db.tableCreate('strikes').run();
+            }
+
+            bot.loadCommands = false;
+            bot.useCommands = true;
+
+            logger.info(`${bot.user.username} is online and ready to cut shit I guess.`);
+            logger.info(`-- ${bot.guilds.size} guilds, ${Object.keys(bot.channelGuildMap).length} channels, ${bot.users.size} users --`);
+            logger.info(`-- ${bot.shards.size} shards`);
         } else {
-            logger.info('Reconnected from Discord');
+            logger.info('Reconnected to Discord');
         }
 
         bot.setGame();
