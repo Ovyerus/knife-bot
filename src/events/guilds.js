84732eacff329909c543fd1aabe380288c03a6ad
Ovyerus <iamovyerus@gmail.com>
2017-06-12 15:35:02 +1000

    Don't be retarded and post guild count when leaving bot collection

--- src/events/guilds.js
+++ src/events/guilds.js
@@ -9,8 +9,12 @@ module.exports = bot => {
         }
     });
 
-    bot.on('guildDelete', () => {
-        bot.editStatus('online', {name: `${bot.currentGame} | ${bot.guilds.size} servers`});
-        bot.postGuildCount();
+    bot.on('guildDelete', g => {
+        if (g.members.filter(m => m.bot).length >= Math.ceil(g.memberCount / 2)) {
+            return;
+        } else {
+            bot.editStatus('online', {name: `${bot.currentGame} | ${bot.guilds.size} servers`});
+            bot.postGuildCount();
+        }
     });
 };
