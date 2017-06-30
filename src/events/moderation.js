9a8328201b7add82d2ff824ca4738fc46145c86b
Ovyerus <iamovyerus@gmail.com>
2017-06-30 21:37:31 +1000

    FINALLY FUCKING FIX EXCEPTIONS BEING BORK

--- src/events/moderation.js
+++ src/events/moderation.js
@@ -1,7 +1,7 @@
 const Actions = ['kicked', 'banned'];
 const Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const InviteRegex = /(?:https:\/\/)?(?:discord\.gg|discordapp\.com\/invite)\/((?:[A-Za-z0-9-])+)/i;
-const DiacriticRegex = /[\u{0300}-\u{036F}\u{0489}]/u;
+const DiacriticRegex = /[\u{0300}-\u{036F}\u{0489}]/u; // eslint-disable-line
 
 module.exports = bot => {
     bot.on('invites', async msg => {
@@ -10,30 +10,28 @@ module.exports = bot => {
         let settings = await bot.getSettings(msg.channel.guild.id);
 
         if (!settings.invites.enabled) return;
-        
-        if (noExcepts(settings) || !userExcept(settings, msg.author.id) || !roleExcept(settings, msg) || !channelExcept(settings, msg.channel.id)) {
+        if (userExcept(settings, msg.author.id) || roleExcept(settings, msg) || channelExcept(settings, msg.channel.id)) return;
+
+        try {
             let invCode = msg.content.match(InviteRegex)[1];
+            let inv = await bot.getInvite(invCode);
+            
+            if (inv.guild.id === msg.channel.guild.id) return;
 
-            try {
-                let inv = await bot.getInvite(invCode);
-
-                if (inv.guild.id === msg.channel.guild.id) return;
-                
-                await msg.delete();
-                await punishChain(bot, msg, settings, 'invites');
-            } catch(err) {
-                if (err.response && typeof err.response === 'string' && JSON.parse(err.response).message === 'Unknown Invite') {
-                    if (settings.invites.fake && noExcepts(settings)) {
-                        try {
-                            await msg.delete();
-                            await punishChain(bot, msg, settings, 'invites');
-                        } catch(err) {
-                            logger.error(err.stack);
-                        }
+            await msg.delete();
+            await punishChain(bot, msg, settings, 'invites');
+        } catch(err) {
+            if (err.response && typeof err.response === 'string' && JSON.parse(err.response).message === 'Unknown Invite') {
+                if (settings.invites.fake) {
+                    try {
+                        await msg.delete();
+                        await punishChain(bot, msg, settings, 'invites');
+                    } catch(err) {
+                        logger.error(err.stack);
                     }
-                } else {
-                    logger.error(err.stack);
                 }
+            } else {
+                logger.error(err.stack);
             }
         }
     });
@@ -44,10 +42,11 @@ module.exports = bot => {
         let settings = await bot.getSettings(msg.channel.guild.id);
 
         if (!settings.mentions.enabled) return;
+        if (userExcept(settings, msg.author.id) || roleExcept(settings, msg) || channelExcept(settings, msg.channel.id)) return;
 
         let mentions = msg.mentions.filter(u => u.id !== msg.author.id && !u.bot);
 
-        if (mentions.length >= settings.mentions.trigger && (noExcepts(settings) || !userExcept(settings, msg.author.id) || !roleExcept(settings, msg) || !channelExcept(settings, msg.channel.id))) {
+        if (mentions.length >= settings.mentions.trigger) {
             try {
                 await msg.delete();
                 await punishChain(bot, msg, settings, 'mentions', `(${mentions.length} mentions)`);
@@ -58,7 +57,7 @@ module.exports = bot => {
     });
 
     bot.on('log', e => {
-        if (!e.settings.logChannel || !e.guild.channels.get(e.settings.logChannel) || !bot.hasPermission('sendMessages', e.guild.channels.get(e.settings.logChannel))) return;
+        if (!e.settings.logChannel || !e.guild.channels.get(e.settings.logChannel) || !e.guild.channels.get(e.settings.logChannel).permissionsOf(bot.user.id).has('sendMessages')) return;
 
         let now = new Date();
         let timestamp = `${now.getUTCDate()} ${Months[now.getUTCMonth()]} ${now.getUTCFullYear()} ${now.getUTCHours()}:${now.getUTCMinutes()}:${now.getUTCSeconds()} UTC`;
@@ -76,24 +75,18 @@ module.exports = bot => {
     });
 };
 
-function noExcepts(res) {
-    return res.exceptions.channels.length === 0 && res.exceptions.roles.length === 0 && res.exceptions.users.length === 0;
-}
-
-function channelExcept(res, channel) {
-    return res.exceptions.channels.includes(channel);
+function userExcept(res, user) {
+    return res.exceptions.users.includes(user);
 }
 
 function roleExcept(res, msg) {
-    for (let role of msg.member.roles) {
-        if (res.exceptions.roles.includes(role)) return true;
-    }
+    for (let role of msg.member.roles) if (res.exceptions.roles.includes(role)) return true;
 
     return false;
 }
 
-function userExcept(res, user) {
-    return res.exceptions.users.includes(user);
+function channelExcept(res, channel) {
+    return res.exceptions.channels.includes(channel);
 }
 
 async function punishChain(bot, msg, settings, type, extra) {
