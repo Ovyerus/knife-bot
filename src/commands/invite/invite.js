db604a4c1fd8a8b77bf816cebf920dbbd188bbf7
Ovyerus <iamovyerus@gmail.com>
2017-07-01 21:07:32 +1000

    Fix invite message

--- src/commands/invite/invite.js
+++ src/commands/invite/invite.js
@@ -3,7 +3,7 @@ exports.commands = ['invite'];
 exports.invite = {
     desc: 'Invite me to your server.',
     main(bot, ctx) {
-        return ctx.createMessage(`**OAuth Invite URL (Recommended Permissions):** <https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=470150215>\n'
-        + '**Support Server:** https://discord.gg/hD8xZMG`);
+        return ctx.createMessage(`**OAuth Invite URL (Recommended Permissions):** <https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=470150215>\n`
+        + '**Support Server:** https://discord.gg/hD8xZMG');
     }
 };
