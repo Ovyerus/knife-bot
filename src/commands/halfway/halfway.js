0997e43da292e2376599540688d051729380fbb9
Ovyerus <iamovyerus@gmail.com>
2017-07-07 21:41:01 +1000

    Make vs and halfway use lookups

--- src/commands/halfway/halfway.js
+++ src/commands/halfway/halfway.js
@@ -5,65 +5,54 @@ exports.halfway = {
     usage: '<user mention|user id>',
     aliases: ['softban'],
     permissions: {both: 'banMembers'},
-    main(bot, ctx) {
-        return new Promise((resolve, reject) => {
-            if (ctx.mentionStrings.length > 0) {
-                softbanMember(ctx).then(resolve).catch(reject);
-            } else if (/^\d+$/.test(ctx.args[0])) {
-                if (ctx.guild.members.get(ctx.args[0])) {
-                    ctx.mentionStrings.push(ctx.args[0]);
-                    softbanMember(ctx).then(resolve).catch(reject);
-                } else {
-                    ctx.createMessage('That user does not exist in this server.').then(resolve).catch(reject);
-                }
-            } else {
-                ctx.createMessage('Please mention someone or give me an ID for me to cut through.').then(resolve).catch(reject);
+    async main(bot, ctx) {
+        if (!ctx.raw) return await ctx.createMessage('Please give me a user to cut through.');
+
+        let user = await bot.lookups.memberLookup(ctx, ctx.raw, false);
+
+        if (!user && !isNaN(ctx.raw)) {
+            try {
+                user = await bot.rest.getRESTUser(ctx.raw);
+            } catch(e) {
+                return await ctx.createMessage('User not found.');
             }
-        });
+        }
+
+        await softBanMember(user, ctx);
     }
 };
 
-function softbanMember(ctx) {
-    return new Promise((resolve, reject) => {
-        let mentionedUser = ctx.client.users.get(ctx.mentionStrings[0]);
-        if (ctx.mentionStrings[0] !== ctx.client.user.id && ctx.mentionStrings[0] !== ctx.author.id) {
-            let mentionMember = ctx.guild.members.get(ctx.mentionStrings[0]);
+async function softBanMember(user, ctx) {
+    if (user.id !== ctx.client.user.id && user.id !== ctx.author.id) {
+        let userTopRolePos = ctx.guild.roles.get(ctx.member.roles.sort((a, b) => {
+            return ctx.guild.roles.get(b).position - ctx.guild.roles.get(a).position;
+        })[0]);
+        userTopRolePos = userTopRolePos ? userTopRolePos.position : 0;
 
-            if (!mentionMember) {
-                ctx.createMessage('That user does not seem to exist.').then(resolve).catch(reject);
-                return;
+        let mentionTopRolePos = ctx.guild.roles.get(user.roles.sort((a, b) => {
+            return ctx.guild.roles.get(b).position - ctx.guild.roles.get(a).position;
+        })[0]);
+        mentionTopRolePos = mentionTopRolePos ? mentionTopRolePos.position : 0;
+
+        if (ctx.author.id === ctx.guild.ownerID || (userTopRolePos > mentionTopRolePos && userTopRolePos !== mentionTopRolePos)) {
+            try {
+                await ctx.guild.banMember(user.id, 7, ctx.client.formatUser(ctx.author));
+                await ctx.guild.unbanMember(user.id, ctx.client.formatUser(ctx.author));
+                await ctx.createMessage(`Cut halfway through **${ctx.client.formatUser(user)}**!`);
+            } catch(err) {
+                if (err.resp && err.resp.statusCode === 403) {
+                    return await ctx.createMessage(`**${ctx.client.formatUser(user)}** is too tough for me and I was unable to be cut through.`);
+                } else {
+                    throw err;
+                }
             }
-
-            let userTopRolePos = ctx.guild.roles.get(ctx.member.roles.sort((a, b) => {
-                return ctx.guild.roles.get(b).position - ctx.guild.roles.get(a).position;
-            })[0]);
-            userTopRolePos = userTopRolePos ? userTopRolePos.position : 0;
-
-            let mentionTopRolePos = ctx.guild.roles.get(mentionMember.roles.sort((a, b) => {
-                return ctx.guild.roles.get(b).position - ctx.guild.roles.get(a).position;
-            })[0]);
-            mentionTopRolePos = mentionTopRolePos ? mentionTopRolePos.position : 0;
-
-            if (ctx.author.id === ctx.guild.ownerID || (userTopRolePos > mentionTopRolePos && userTopRolePos !== mentionTopRolePos)) {
-                ctx.guild.banMember(ctx.mentionStrings[0], 7).then(() => {
-                    return ctx.guild.unbanMember(ctx.mentionStrings[0]);
-                }).then(() => {
-                    return ctx.createMessage(`Cut halfway through **${ctx.client.formatUser(mentionedUser)}**!`);
-                }).then(resolve).catch(err => {
-                    if (err.resp && err.resp.statusCode === 403) {
-                        return ctx.createMessage(`**${ctx.client.formatUser(mentionedUser)}** is too tough for me and I was unable to be cut through.`);
-                    } else {
-                        reject(err);
-                        return null;
-                    }
-                }).then(res => {if (res) resolve(res);});
-            } else {
-                ctx.createMessage(`**${ctx.client.formatUser(mentionMember)}** was wearing an anti-1000-degree-knife vest and you were unable to cut through them.\n**(You cannot ban people ${userTopRolePos === mentionTopRolePos ? 'with the same role as you.' : 'higher then you'})**`).then(resolve).catch(reject);
-            }
-        } else if (ctx.mentionStrings[0] === ctx.author.id) {
-            ctx.createMessage("You can't cut yourself.").then(resolve).catch(reject);
-        } else if (ctx.mentionStrings[0] === ctx.client.user.id) {
-            ctx.createMessage("You can't cut the knife with itself.").then(resolve).catch(reject);
+        } else {
+            await ctx.createMessage(`**${ctx.client.formatUser(user)}** was wearing an anti-1000-degree-knife vest and you were unable to cut through them.\n`
+            + `**(You cannot ban people ${userTopRolePos === mentionTopRolePos ? 'with the same role as you.' : 'higher then you'})**`);
         }
-    });
+    } else if (user.id === ctx.author.id) {
+        await ctx.createMessage("You can't cut yourself.");
+    } else if (user.id === ctx.client.user.id) {
+        await ctx.createMessage("You can't cut the knife with itself.");
+    }
 }
--- src/commands/vs/vs.js
+++ src/commands/vs/vs.js
@@ -6,62 +6,52 @@ exports.vs = {
     aliases: ['ban'],
     permissions: {both: 'banMembers'},
     async main(bot, ctx) {
-        if (ctx.mentionStrings.length > 0) {
-            await banMember(ctx);
-        } else if (/^\d+$/.test(ctx.args[0])) {
-            if (ctx.guild.members.get(ctx.args[0])) {
-                ctx.mentionStrings.push(ctx.args[0]);
-                await banMember(ctx);
-            } else {
-                let user = await bot.rest.getRESTUser(ctx.args[0]);
-                await ctx.guild.banMember(user.id);
-                await ctx.createMessage(`Cut all the way through **${bot.formatUser(user)}** from behind!`);
+        if (!ctx.raw) return await ctx.createMessage('Please give me a user to cut through.');
+
+        let user = await bot.lookups.memberLookup(ctx, ctx.raw, false);
+
+        if (!user && !isNaN(ctx.raw)) {
+            try {
+                user = await bot.rest.getRESTUser(ctx.raw);
+            } catch(e) {
+                return await ctx.createMessage('User not found.');
             }
-        } else {
-            await ctx.createMessage('Please mention someone or give me an ID for me to cut through.');
         }
+
+        await banMember(user, ctx);
     }
 };
 
-async function banMember(ctx) {
-    let mentionedUser = ctx.client.users.get(ctx.mentionStrings[0]);
-
-    if (ctx.mentionStrings[0] !== ctx.client.user.id && ctx.mentionStrings[0] !== ctx.author.id) {
-        let mentionMember = ctx.guild.members.get(ctx.mentionStrings[0]);
-
-        if (!mentionMember) {
-            await ctx.createMessage('That user does not seem to exist.');
-            return;
-        }
-
+async function banMember(user, ctx) {
+    if (user.id !== ctx.client.user.id && user.id !== ctx.author.id) {
         let userTopRolePos = ctx.guild.roles.get(ctx.member.roles.sort((a, b) => {
             return ctx.guild.roles.get(b).position - ctx.guild.roles.get(a).position;
         })[0]);
         userTopRolePos = userTopRolePos ? userTopRolePos.position : 0;
 
-        let mentionTopRolePos = ctx.guild.roles.get(mentionMember.roles.sort((a, b) => {
+        let mentionTopRolePos = ctx.guild.roles.get(user.roles.sort((a, b) => {
             return ctx.guild.roles.get(b).position - ctx.guild.roles.get(a).position;
         })[0]);
         mentionTopRolePos = mentionTopRolePos ? mentionTopRolePos.position : 0;
 
         if (ctx.author.id === ctx.guild.ownerID || (userTopRolePos > mentionTopRolePos && userTopRolePos !== mentionTopRolePos)) {
             try {
-                await ctx.guild.banMember(ctx.mentionStrings[0], 7, ctx.client.formatUser(ctx.author));
-                await ctx.createMessage(`Cut all the way through **${ctx.client.formatUser(mentionedUser)}**!`);
+                await ctx.guild.banMember(user.id, 7, ctx.client.formatUser(ctx.author));
+                await ctx.createMessage(`Cut all the way through **${ctx.client.formatUser(user)}**!`);
             } catch(err) {
                 if (err.resp && err.resp.statusCode === 403) {
-                    return await ctx.createMessage(`**${ctx.client.formatUser(mentionedUser)}** is too tough for me and I was unable to be cut through.`);
+                    return await ctx.createMessage(`**${ctx.client.formatUser(user)}** is too tough for me and I was unable to be cut through.`);
                 } else {
                     throw err;
                 }
             }
         } else {
-            await ctx.createMessage(`**${ctx.client.formatUser(mentionMember)}** was wearing an anti-1000-degree-knife vest and you were unable to cut through them.\n`
+            await ctx.createMessage(`**${ctx.client.formatUser(user)}** was wearing an anti-1000-degree-knife vest and you were unable to cut through them.\n`
             + `**(You cannot ban people ${userTopRolePos === mentionTopRolePos ? 'with the same role as you.' : 'higher then you'})**`);
         }
-    } else if (ctx.mentionStrings[0] === ctx.author.id) {
+    } else if (user.id === ctx.author.id) {
         await ctx.createMessage("You can't cut yourself.");
-    } else if (ctx.mentionStrings[0] === ctx.client.user.id) {
+    } else if (user.id === ctx.client.user.id) {
         await ctx.createMessage("You can't cut the knife with itself.");
     }
 }
