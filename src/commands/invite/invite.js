9f246217f4fc9f9b81dee5dd7a975e1723bada07
Ovyerus <iamovyerus@gmail.com>
2017-08-05 20:42:39 +1000

    Update server url to new server

--- src/commands/invite/invite.js
+++ src/commands/invite/invite.js
@@ -4,6 +4,6 @@ exports.invite = {
     desc: 'Invite me to your server.',
     main(bot, ctx) {
         return ctx.createMessage(`**OAuth Invite URL (Recommended Permissions):** <https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=470150215>\n`
-        + '**Support Server:** https://discord.gg/hD8xZMG');
+        + '**Support Server:** https://discord.gg/G9nUSZt');
     }
 };
