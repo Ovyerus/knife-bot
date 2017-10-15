48bbfe500b7df03c47d792dbeff03f7bf4f68788
Ovyerus <Ovyerus@users.noreply.github.com>
2017-10-15 19:40:27 +1100

    Github pages bois

--- src/commands/invite/invite.js
+++
@@ -1,9 +0,0 @@
-exports.commands = ['invite'];
-
-exports.invite = {
-    desc: 'Invite me to your server.',
-    main(bot, ctx) {
-        return ctx.createMessage(`**OAuth Invite URL (Recommended Permissions):** <https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=470150215>\n`
-        + '**Support Server:** https://discord.gg/G9nUSZt');
-    }
-};
