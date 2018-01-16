b43b03db61025e69a31e69a0157354ff2884b642
Ovyerus <iamovyerus@gmail.com>
2018-01-16 16:24:23 +1100

    Add function to get redirects for links given to it.

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,3 +1,5 @@
+const got = require('got');
+
 const MONTHS = [
     'January',
     'February',
@@ -39,4 +41,23 @@ function LCG(seed) {
     return () => (seed = lcg(seed)) / 2147483648;
 }
 
-module.exports = {AwaitTimeout, ValueError, formatUTC, LCG};
+async function getLinkRedirects(url) {
+    if (typeof url !== 'string') throw new TypeError('url is not a string.');
+
+    let res = await got(`http://www.getlinkinfo.com/info?link=${encodeURIComponent(url)}`);
+    let body = res.body.replace(/\r|\n/g, '');
+
+    if (/id="statusMessage"/.test(body)) throw new Error('Bad URL');
+    if (/class="error-details"/.test(body)) throw new Error(body.match(/<dd class="error-details">(.*?)<\/dd>/)[1]);
+    if (/<dd class="redirections-list">.*?\(none\).*?<\/dd>/.test(body)) return [];
+
+    let redirects = body.match(/<dd class="redirections-list">.*?<ol>(.*?)<\/ol>.*?<\/dd>/);
+
+    return redirects ? redirects[1].match(/<li>(.*?)<\/li>/g).map(v => v.match(/<a href="(.*?)".*?>.*?<\/a>/)[1]) : [];
+}
+
+const URLRegex = /(?:(?:https?:)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?/gi;
+
+const inviteRegex = /(?:https?:\/\/)?(?:discord\.gg|discordapp\.com\/invite)\/\s*?((?:[A-Za-z0-9]|-)+)/i;
+
+module.exports = {AwaitTimeout, ValueError, formatUTC, LCG, inviteRegex, URLRegex, getLinkRedirects};
