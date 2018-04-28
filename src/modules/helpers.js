00c1ee1130f2cd00aee13e3b3b2cd09c35cc9bd8
Ovyerus <iamovyerus@gmail.com>
2018-04-28 22:01:32 +1000

    Yay for different line endings

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -81,7 +81,28 @@ async function getLinkRedirects(url) {
 }
 
 const URLRegex = /(?:(?:https?:)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?/gi;
-
 const inviteRegex = /(?:https?:\/\/)?(?:discord\.gg|discordapp\.com\/invite)\/\s*?((?:[A-Za-z0-9]|-)+)/i;
 
-module.exports = {AwaitTimeout, ValueError, formatUTC, LCG, inviteRegex, URLRegex, getLinkRedirects};
+const userFriendlyHelpers = {
+    /**
+     * Easy method for checking if a string has a similar meaning to enable.
+     * 
+     * @param {String} str String to check.
+     * @returns {Boolean} Whether the string has a similar meaning to enable.
+     */
+    isEnable(str) {
+        return ['enable', 'true', 'on', 'activate', 'active', 'turn on', 'allow', 'yes', 'y'].includes(str.toLowerCase());
+    },
+
+    /**
+     * Easy method for checking is a string has a similar meaning to disable.
+     * 
+     * @param {String} str String to check.
+     * @returns {Boolean} Whether the string has a similar meaning to disable.
+     */
+    isDisable(str) {
+        return ['disable', 'false', 'off', 'deactivate', 'inactive', 'turn off', 'disallow', 'no', 'n'].includes(str.toLowerCase());
+    }
+};
+
+module.exports = {AwaitTimeout, ValueError, formatUTC, LCG, inviteRegex, URLRegex, getLinkRedirects, userFriendlyHelpers};
